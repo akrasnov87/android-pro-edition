ec69bfe58865e72eb7924a4257b10541e5678938
Александр Краснов <a-krasnov@it-serv.ru>
2022-06-10 06:14:35 +0300

    temp

--- app/src/main/java/com/mobwal/pro/models/LocationInfo.java
+++ app/src/main/java/com/mobwal/pro/models/LocationInfo.java
@@ -55,7 +55,7 @@ public class LocationInfo
             myLocation.setLongitude(taskLongitude);
             myLocation.setLatitude(taskLatitude);
 
-            return Long.valueOf(String.valueOf(myLocation.distanceTo(attachLocation)));
+            return (long)0;// Long.valueOf(String.valueOf(myLocation.distanceTo(attachLocation)));
         }
 
         return null;
